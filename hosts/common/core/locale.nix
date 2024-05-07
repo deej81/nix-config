ee85caa2e5ba73a5d84c7a1adc468b44ecccac87
Dan Jordan <deej81@users.noreply.github.com>
2024-05-07 08:02:08 -0600

    fix locale

--- hosts/common/core/locale.nix
+++ hosts/common/core/locale.nix
@@ -1,4 +1,4 @@
 { lib, ... }: {
-  i18n.defaultLocale = lib.mkDefault "en_US.UTF-8";
-  time.timeZone = lib.mkDefault "America/Edmonton";
+  i18n.defaultLocale = lib.mkDefault "en_GB.UTF-8";
+  time.timeZone = lib.mkDefault "Europe/London";
 }
