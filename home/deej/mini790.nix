a1d8848d034b71334825298527350361572e1109
Dan Jordan <deej81@users.noreply.github.com>
2024-05-14 08:45:58 +0100

    add mini790 fix 1password browser extension link

---
+++ home/deej/mini790.nix
@@ -0,0 +1,28 @@
+{ configVars, ... }:
+{
+  imports = [
+    #################### Required Configs ####################
+    common/core #required
+
+    #################### Host-specific Optional Configs ####################
+    
+    common/optional/helper-scripts
+
+    common/optional/desktops
+    common/optional/dev
+
+    common/optional/browsers/brave.nix
+    common/optional/browsers/firefox.nix
+
+    common/optional/comms/slack.nix
+
+    common/optional/tools/_1password-ssh.nix
+
+
+  ];
+
+  home = {
+    username = configVars.username;
+    homeDirectory = "/home/${configVars.username}";
+  };
+}
