8d783c2824faf4ac0ef53ba9ddfbb694dc8db200
Dan Jordan <deej81@users.noreply.github.com>
2024-06-26 09:29:45 +0100

    tidy

--- hosts/common/core/sops.nix
+++ hosts/common/core/sops.nix
@@ -3,22 +3,22 @@
 { inputs, config, ... }:
 
 {
-    imports = [
-        inputs.sops-nix.nixosModules.sops
-    ];
+  imports = [
+    inputs.sops-nix.nixosModules.sops
+  ];
 
-    sops = {
-        defaultSopsFile = ../../../secrets.yml;
-        validateSopsFiles = false;
-        
-        age = {
-            sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-            keyFile = "/var/lib/sops-nix/age.key";
-            generateKey = true;
-        };
+  sops = {
+    defaultSopsFile = ../../../secrets.yml;
+    validateSopsFiles = false;
 
-        secrets = {
-            github-email = {};
-        };
+    age = {
+      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+      keyFile = "/var/lib/sops-nix/age.key";
+      generateKey = true;
     };
+
+    secrets = {
+      github-email = { };
+    };
+  };
 }
