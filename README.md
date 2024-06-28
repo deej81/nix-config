fe298a4a5af1561982ddbc72f328ad3112779abb
Dan Jordan <deej81@users.noreply.github.com>
2024-06-28 10:35:57 +0100

    add firefox config

--- README.md
+++ README.md
@@ -3,18 +3,18 @@
 
 ## issues
 
-
 - [ ] fix desktops "0"
 - [ ] fix monitor per host config hyprland
 - [ ] fix - enable ntp
-- [ ] fix firefox config
+- [x] fix firefox config
 - [ ] fix swaync summary
 - [ ] refactor hyprland setup into a single "desktop" package
 - [ ] fix waybar power icon action
 - [ ] fix weather
 - [ ] auto format nix files
+- [ ] fix LUKS prompt appearing before keyboard is recognised
+- [ ] fix warnings after upgrade
  
-
 ## Acknowledgements
 
 Based off EmergentMind's config https://github.com/EmergentMind/nix-config. There is a really nice youtube video that goes along with this, highly recommend it. 
--- home/deej/common/optional/browsers/firefox.nix
+++ home/deej/common/optional/browsers/firefox.nix
@@ -1,5 +1,79 @@
-{
-  programs.firefox = {
-    enable = true;
+{ config, pkgs, ... }:
+
+  let
+    lock-false = {
+      Value = false;
+      Status = "locked";
+    };
+    lock-true = {
+      Value = true;
+      Status = "locked";
+    };
+  in{
+  programs = {
+    firefox = {
+      enable = true;
+
+      /* ---- POLICIES ---- */
+      # Check about:policies#documentation for options.
+      policies = {
+        DisableTelemetry = true;
+        DisableFirefoxStudies = true;
+        EnableTrackingProtection = {
+          Value= true;
+          Locked = true;
+          Cryptomining = true;
+          Fingerprinting = true;
+        };
+        DisablePocket = true;
+        DisableFirefoxAccounts = true;
+        DisableAccounts = true;
+        DisableFirefoxScreenshots = true;
+        OverrideFirstRunPage = "";
+        OverridePostUpdatePage = "";
+        DontCheckDefaultBrowser = true;
+        DisplayBookmarksToolbar = "newtab"; # alternatives: "always" or "newtab"
+        DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
+        SearchBar = "unified"; # alternative: "separate"
+
+        /* ---- EXTENSIONS ---- */
+        # Check about:support for extension/add-on ID strings.
+        # Valid strings for installation_mode are "allowed", "blocked",
+        # "force_installed" and "normal_installed".
+        ExtensionSettings = {
+          # 1Password:
+          "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
+            installation_mode = "force_installed";
+          };
+        };
+  
+        /* ---- PREFERENCES ---- */
+        # Check about:config for options.
+        Preferences = { 
+          "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
+          "extensions.pocket.enabled" = lock-false;
+          "extensions.screenshots.disabled" = lock-true;
+          "browser.topsites.contile.enabled" = lock-false;
+          "browser.formfill.enable" = lock-false;
+          "browser.search.suggest.enabled" = lock-false;
+          "browser.search.suggest.enabled.private" = lock-false;
+          "browser.urlbar.suggest.searches" = lock-false;
+          "browser.urlbar.showSearchSuggestionsFirst" = lock-false;
+          "browser.newtabpage.activity-stream.feeds.section.topstories" = lock-false;
+          "browser.newtabpage.activity-stream.feeds.snippets" = lock-false;
+          "browser.newtabpage.activity-stream.section.highlights.includePocket" = lock-false;
+          "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = lock-false;
+          "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = lock-false;
+          "browser.newtabpage.activity-stream.section.highlights.includeVisited" = lock-false;
+          "browser.newtabpage.activity-stream.showSponsored" = lock-false;
+          "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
+          "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
+          "browser.sessionstore.resume_from_crash" = lock-false;
+          "browser.bookmarks.file" = ./bookmarks.html;
+          "browser.places.importBookmarksHTML" = true;
+        };
+      };
+    };
   };
 }
