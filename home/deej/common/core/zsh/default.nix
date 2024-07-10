ebbd117f25298fd27b9e7dae2e57eef3250a78c1
Dan Jordan <deej81@users.noreply.github.com>
2024-07-10 14:30:34 +0100

    convert to starship terminal

--- home/deej/common/core/zsh/default.nix
+++ home/deej/common/core/zsh/default.nix
@@ -1,4 +1,125 @@
 { pkgs, ... }: {
+
+  # Based on ChrisTutusTech's prompt
+  programs.starship = {
+    enable = true;
+    settings = {
+      format = ''[](#3B4252)$python$username[](bg:#434C5E fg:#3B4252)$directory[](fg:#434C5E bg:#4C566A)$git_branch$git_status[](fg:#4C566A bg:#86BBD8)$c$elixir$elm$golang$haskell$java$julia$nodejs$nim$rust[](fg:#86BBD8 bg:#06969A)$docker_context[](fg:#06969A bg:#33658A)$time[ ](fg:#33658A)'';
+
+      command_timeout = 5000;
+
+      username = {
+        show_always = true;
+        style_user = "bg:#3B4252";
+        style_root = "bg:#3B4252";
+        format = "[$user ]($style)";
+      };
+
+      directory = {
+        style = "bg:#434C5E";
+        format = "[ $path ]($style)";
+        truncation_length = 3;
+        truncation_symbol = "…/";
+        substitutions = {
+          Documents = "󰈙 ";
+          Downloads = " ";
+          Music = " ";
+          Pictures = " ";
+        };
+      };
+
+      c = {
+        symbol = " ";
+        style = "bg:#86BBD8";
+        format = "[ $symbol ($version) ]($style)";
+      };
+
+      docker_context = {
+        symbol = " ";
+        style = "bg:#06969A";
+        format = "[ $symbol $context ]($style) $path";
+      };
+
+      elixir = {
+        symbol = " ";
+        style = "bg:#86BBD8";
+        format = "[ $symbol ($version) ]($style)";
+      };
+
+      elm = {
+        symbol = " ";
+        style = "bg:#86BBD8";
+        format = "[ $symbol ($version) ]($style)";
+      };
+
+      git_branch = {
+        symbol = "";
+        style = "bg:#4C566A";
+        format = "[ $symbol $branch ]($style)";
+      };
+
+      git_status = {
+        style = "bg:#4C566A";
+        format = "[$all_status$ahead_behind ]($style)";
+      };
+
+      golang = {
+        symbol = " ";
+        style = "bg:#86BBD8";
+        format = "[ $symbol ($version) ]($style)";
+      };
+
+      haskell = {
+        symbol = " ";
+        style = "bg:#86BBD8";
+        format = "[ $symbol ($version) ]($style)";
+      };
+
+      java = {
+        symbol = " ";
+        style = "bg:#86BBD8";
+        format = "[ $symbol ($version) ]($style)";
+      };
+
+      julia = {
+        symbol = " ";
+        style = "bg:#86BBD8";
+        format = "[ $symbol ($version) ]($style)";
+      };
+
+      nodejs = {
+        symbol = "";
+        style = "bg:#86BBD8";
+        format = "[ $symbol ($version) ]($style)";
+      };
+
+      nim = {
+        symbol = " ";
+        style = "bg:#86BBD8";
+        format = "[ $symbol ($version) ]($style)";
+      };
+
+      python = {
+        style = "bg:#3B4252";
+        format = "[(\($virtualenv\) )]($style)";
+      };
+
+      rust = {
+        symbol = "";
+        style = "bg:#86BBD8";
+        format = "[ $symbol ($version) ]($style)";
+      };
+
+      time = {
+        disabled = false;
+        time_format = "%R";
+        style = "bg:#33658A";
+        format = "[ $time ]($style)";
+      };
+    };
+  };
+
+
   programs.zsh = {
     enable = true;
 
@@ -12,16 +133,16 @@
     history.share = true;
 
     plugins = [
-      {
-        name = "powerlevel10k-config";
-        src = ./p10k;
-        file = "p10k.zsh";
-      }
-      {
-        name = "zsh-powerlevel10k";
-        src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
-        file = "powerlevel10k.zsh-theme";
-      }
+      # {
+      #   name = "powerlevel10k-config";
+      #   src = ./p10k;
+      #   file = "p10k.zsh";
+      # }
+      # {
+      #   name = "zsh-powerlevel10k";
+      #   src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
+      #   file = "powerlevel10k.zsh-theme";
+      # }
       {
         name = "zsh-term-title";
         src = "${pkgs.zsh-term-title}/share/zsh/zsh-term-title/";
@@ -45,9 +166,9 @@
       # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
       # Initialization code that may require console input (password prompts, [y/n]
       # confirmations, etc.) must go above this block; everything else may go below.
-      if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
-        source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
-      fi
+      # if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+      #   source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+      # fi
     '';
 
     initExtra = ''
