0eb0ade0b3c32a275d393e54dba37c6e53e2f496
Dan Jordan <deej81@users.noreply.github.com>
2024-08-22 14:40:47 +0100

    add thunar

--- hosts/common/optional/hyprland.nix
+++ hosts/common/optional/hyprland.nix
@@ -8,6 +8,17 @@
 
   services.blueman.enable = true;
 
+  programs.thunar = {
+    enable = true;
+    plugins = [
+      pkgs.xfce.thunar-volman
+      pkgs.xfce.thunar-archive-plugin
+      pkgs.xfce.thunar-media-tags-plugin
+    ];
+  };
+
+  programs.file-roller.enable = true;
+  
   environment.systemPackages = [
     pkgs.swaynotificationcenter
     pkgs.swww
