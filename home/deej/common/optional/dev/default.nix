5b6ccf694156afb0291adb9fe9f241cd08a7fdae
Dan Jordan <deej81@users.noreply.github.com>
2024-08-23 13:35:57 +0100

    vscode config

--- home/deej/common/optional/dev/default.nix
+++ home/deej/common/optional/dev/default.nix
@@ -13,6 +13,9 @@
       "files.autoSave" = "onFocusChange";
       "workbench.colorTheme" = "Default Dark Modern";
       "git.suggestSmartCommit" = false;
+      "editor.fontFamily" = "'JetBrainsMono Nerd Font', Consolas, 'Courier New', monospace";
+      "editor.fontLigatures" = true;
+      "editor.fontSize" = 16;
     };
   };
 
