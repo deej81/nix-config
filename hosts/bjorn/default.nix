26aab429cb6791add65e8129d6b3d22442407cdd
Dan Jordan <deej81@users.noreply.github.com>
2024-09-11 09:55:35 +0100

    add windows rdp (remmina)

--- hosts/bjorn/default.nix
+++ hosts/bjorn/default.nix
@@ -30,6 +30,7 @@
     (configLib.relativeToRoot "hosts/common/optional/quickemu.nix")
     (configLib.relativeToRoot "hosts/common/optional/programming/elixir.nix")
     (configLib.relativeToRoot "hosts/common/optional/services/localsend.nix")
+    (configLib.relativeToRoot "hosts/common/optional/windows/rdp.nix")
 
 
     #################### Users to Create ####################
