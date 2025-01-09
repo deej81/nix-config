05da3c0d252e2f629e8409f86ccfa314a6737ba4
Dan Jordan <deej81@users.noreply.github.com>
2025-01-09 16:46:16 +0000

    install aider on nyx

--- hosts/nyx/default.nix
+++ hosts/nyx/default.nix
@@ -25,6 +25,7 @@
     (configLib.relativeToRoot "hosts/common/optional/spotify.nix")
 
     (configLib.relativeToRoot "hosts/common/optional/_1password.nix")
+    (configLib.relativeToRoot "hosts/common/optional/aider.nix")
     (configLib.relativeToRoot "hosts/common/optional/steam.nix")
     (configLib.relativeToRoot "hosts/common/optional/tailscale.nix")
     (configLib.relativeToRoot "hosts/common/optional/docker.nix")
