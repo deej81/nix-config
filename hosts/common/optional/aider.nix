670bb8059d1a6ca5f4117b8bf54b49774726039a
Dan Jordan <deej81@users.noreply.github.com>
2025-04-10 12:59:15 +0100

    update aider

--- hosts/common/optional/aider.nix
+++ hosts/common/optional/aider.nix
@@ -1,14 +1,6 @@
 { pkgs, ... }:
 {
   environment.systemPackages = [
-    (pkgs.unstable.aider-chat.overrideAttrs (oldAttrs: {
-      version = "0.78.0";
-      src = pkgs.fetchFromGitHub {
-        owner = "Aider-AI";
-        repo = "aider";
-        tag = "v0.78.0";
-        hash = "sha256-JXzkvuSOOEUxNqF6l5USzIPftpnIW+CptEv/0yp0eGM=";
-      };
-    }))
+    pkgs.unstable.aider-chat
   ];
 }
