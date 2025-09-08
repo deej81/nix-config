6a482851e399731d32b0bf113a2557476ca6a226
Dan Jordan <deej81@users.noreply.github.com>
2025-09-08 11:14:50 +0100

    add rider fixes

--- home/deej/common/optional/dev/rider.nix
+++ home/deej/common/optional/dev/rider.nix
@@ -3,4 +3,11 @@
   home.packages = [
     pkgs.jetbrains.rider
   ];
+
+  # Hyprland window rules for JetBrains Rider compatibility
+  wayland.windowManager.hyprland.settings.windowrulev2 = [
+    "tag +jb, class:^jetbrains-.+$,floating:1"
+    "stayfocused, tag:jb"
+    "noinitialfocus, tag:jb"
+  ];
 }
